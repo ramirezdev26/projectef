21ff3b0d1ad39174949ca69f01614742209e38b3
Santiago Ramirez <santiago.ramirezc26@gmail.com>
2023-05-17 16:19:01 -0500

    Added the config connection to local sql server database

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using projectef;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddDbContext<TasksContext>(p => p.UseInMemoryDatabase("TasksDB"));
+builder.Services.AddSqlServer<TasksContext>("Data Source=DESKTOP-K21Q9MA\\MSSQLSERVER01; initial Catalog=TasksDb; Trusted_Connection=True; Integrated Security=True; TrustServerCertificate=True");
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconnection", async ([FromServices] TasksContext dbContext) => {
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Databes in memory: " + dbContext.Database.IsInMemory());
+});
 
 app.Run();
