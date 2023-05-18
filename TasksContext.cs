81c8585ece1fc4dfb3213a0a119ca993e779b356
Santiago Ramirez <santiago.ramirezc26@gmail.com>
2023-05-18 11:57:41 -0500

    Added ef migrations

--- TasksContext.cs
+++ TasksContext.cs
@@ -18,6 +18,7 @@ public class TasksContext: DbContext
                 category.HasKey(p => p.CategoryId);
                 category.Property(p => p.Name).IsRequired().HasMaxLength(150);
                 category.Property(p => p.Description);
+                category.Property(p => p.Weight);
             }
         );
 
--- models/Category.cs
+++ models/Category.cs
@@ -12,5 +12,6 @@ public class Category
     public string Name {get;set;}
 
     public string Description {get;set;}
+    public int Weight {get;set;}
     public virtual ICollection<Task> Tasks {get;set;}
 }
