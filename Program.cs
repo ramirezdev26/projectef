5952ce5f0842dac5527d54db25f13fc049064713
Santiago Ramirez <santiago.ramirezc26@gmail.com>
2023-05-18 14:56:08 -0500

    Added CRUD Behaviour to the entry point related with the Task model

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using projectef;
+using Task = projectef.models.Task;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,5 +16,57 @@ app.MapGet("/dbconnection", async ([FromServices] TasksContext dbContext) => {
     dbContext.Database.EnsureCreated();
     return Results.Ok("Databes in memory: " + dbContext.Database.IsInMemory());
 });
+
+app.MapGet("/api/tasks", async ([FromServices] TasksContext dbContext) => 
+{
+    return Results.Ok(dbContext.Tasks.Include(p => p.Category));
+});
+
+app.MapPost("/api/tasks", async ([FromServices] TasksContext dbContext, [FromBody] Task task) => 
+{
+    task.TaskId = Guid.NewGuid();
+    task.CreationDate = DateTime.Now;
+    await dbContext.AddAsync(task);
+
+    await dbContext.SaveChangesAsync();
+
+    return Results.Ok();
+});
+
+app.MapPut("/api/tasks/{id}", async ([FromServices] TasksContext dbContext, [FromBody] Task task, [FromRoute] Guid id) => 
+{
+    var currentTask = dbContext.Tasks.Find(id);
+
+    if(currentTask != null)
+    {
+        currentTask.CategoryId = task.CategoryId;
+        currentTask.Title = task.Title;
+        currentTask.TaskPriority = task.TaskPriority;
+        currentTask.Description = task.Description;
+
+        await dbContext.SaveChangesAsync();
+
+        return Results.Ok();
+    }
+
+
+    return Results.NotFound();
+});
+
+app.MapDelete("api/tasks/{id}", async ([FromServices] TasksContext dbContext, [FromRouteAttribute] Guid id) => 
+{
+    var currentTask = dbContext.Tasks.Find(id);
+
+    if(currentTask != null)
+    {
+        dbContext.Remove(currentTask);
+        await dbContext.SaveChangesAsync();
+
+        return Results.Ok();
+    }
+
+    return Results.NotFound();
+});
+
 
 app.Run();
--- models/Category.cs
+++ models/Category.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace projectef.models;
 
@@ -13,5 +14,7 @@ public class Category
 
     public string Description {get;set;}
     public int Weight {get;set;}
+
+    [JsonIgnore]
     public virtual ICollection<Task> Tasks {get;set;}
 }
